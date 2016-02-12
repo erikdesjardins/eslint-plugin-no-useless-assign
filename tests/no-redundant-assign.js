dd9bb8792862a3d99889770633268fdd691af633
Erik Desjardins <erikdesjardins@users.noreply.github.com>
2016-02-12 11:00:06 -0500

    tests for valid code

--- tests/no-redundant-assign.js
+++ tests/no-redundant-assign.js
@@ -12,6 +12,30 @@ var RuleTester = require("eslint").RuleTester;
 var ruleTester = new RuleTester();
 ruleTester.run('no-redundant-assign', rule, {
 	valid: [
+		// constant
+		'return 5;',
+		// variable
+		'return foo;',
+		// function invocation
+		'return foo.bar();',
+		// property of preceding variable
+		'var foo; return foo.bar;',
+		// function invocation of preceding variable
+		'var foo; return foo();',
+		// function invocation on preceding variable
+		'var foo; return foo.bar()',
+		// expression involving preceding variable
+		'var foo; return foo + bar;',
+		// function invocation with preceding variable as argument
+		'var foo; return bar(foo);',
+		// assignment of out-of-scope var
+		'var foo; function() { foo = bar; return foo; };',
+		// assignment of out-of-scope var, arrow function
+		{ code: 'var foo; () => { foo = bar; return foo; };', ecmaFeatures: { arrowFunctions: true } },
+		// assignment of out-of-scope let
+		{ code: 'let foo; function() { foo = bar; return foo; };', ecmaFeatures: { blockBindings: true } },
+		// assignment of out-of-scope let, arrow function
+		{ code: 'let foo; () => { foo = bar; return foo; };', ecmaFeatures: { arrowFunctions: true, blockBindings: true } }
 	],
 	invalid: [
 	]
